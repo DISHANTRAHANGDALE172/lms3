ac4502bc24827c31d4a40f9d8151303bd52b06e4
dishant <rdishant035@gmail.com>
2023-03-29 22:18:38 +0530

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react'
+
 import './App.css';
+import Navbar from './componenets/Navbar';
+import Show from './componenets/Show';
+import Summary from './componenets/Summary';
+
+import {
+	BrowserRouter,
+	Routes,
+	Route,
+	
+} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [shows, setshows] = useState([]);
+	
+	useEffect(() => {
+		const updateshows = async () => {
+			const url = "https://api.tvmaze.com/search/shows?q=all";
+			const data = await fetch(url);
+			const parseddata = await data.json();
+			
+			// parseddata.pop()
+	// parseddata.forEach(element => {
+	// 	console.log(element.show.summary);
+		
+	// });
+			setshows(parseddata);
+		}
+
+		updateshows();
+
+	}, []);
+
+	return (
+		<BrowserRouter>
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<Show   shows={shows}/>} />
+				<Route path="/home" element={<Show  shows={shows}/>} />
+				<Route path="/summary" element={<Summary data={shows} />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
---
+++ src/componenets/Showitem.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {
+	
+	Link,
+  } from "react-router-dom";
+export default function Showitem(props) {
+	return (
+		<div>
+			<div className="card" >
+				<img src={props.image === null ? '#' : props.image.original} className="card-img-top" alt="no" />
+				<div className="card-body">
+					<h5 className="card-title">{props.title}</h5>
+					<p className="card-text">{props.des}</p>
+					<div className="d-flex justify-content-around">
+						<a href={props.u} target="_blank" rel="noopener noreferrer" className="btn btn-sm btn-primary">Read More</a>
+						<Link className="nav-link active"  to="/summary">Summary</Link>
+						{/* <a href={props.u} target="_blank" rel="noopener noreferrer" className="btn btn-sm btn-primary">Summary</a> */}
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
